ed3eed272ef52ea2c11a2f88d3980ad40f2e059d
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-04-02 06:17:42 +0300

    Added demo project

---
+++ Demo/SwiftyPredictorDemo/SwiftyPredictorDemo/ViewControllers/Main/MainViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MainViewController.swift
+//  SwiftyPredictorDemo
+//
+//  Created by Igor Matyushkin on 02.04.17.
+//  Copyright © 2017 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Outlets
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Private object methods
+    
+    // MARK: Actions
+    
+    // MARK: Protocol methods
+    
+}
